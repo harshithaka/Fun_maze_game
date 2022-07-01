970fcfec9395a06532d8bee2b82e2300c4276888
harshithaka <37140786+harshithaka@users.noreply.github.com>
2022-07-01 11:45:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,6 @@
 
 (function(){
-    // Import the functions you need from the SDKs you need
-    // import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-    // import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-analytics.js";
-    // TODO: Add SDKs for Firebase products that you want to use
-    // https://firebase.google.com/docs/web/setup#available-libraries
-  
+ 
     // Your web app's Firebase configuration
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     const firebaseConfig = {
